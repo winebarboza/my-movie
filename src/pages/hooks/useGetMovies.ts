bb6c7983efbc929940fbe28b7ec8b389b30156ab
winebarboza <wine.barbosa@gmail.com>
2023-08-15 21:59:26 -0300

    hook responsável por requisição a api

---
+++ src/pages/hooks/useGetMovies.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { Movie, MovieApiResponse } from "../../models/movie.interface";
+import api from "../../providers";
+const useGetMovies = (endpoint: string) => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await api.get(endpoint);
+                const data: MovieApiResponse = response.data;
+                setMovies(data.results)
+            } catch (error) {
+                console.error("error fetching movies", error)
+            }
+        };
+        fetchMovies()
+    }, [endpoint]);
+    return movies;
+};
+export default useGetMovies;
