36ab80cfe1ae6260a67364e02a7b7c74cde11f97
winebarboza <wine.barbosa@gmail.com>
2023-08-16 12:43:50 -0300

    style - adicionando linha vazia para organização do código

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import InitialPage from './pages/initialPage';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
