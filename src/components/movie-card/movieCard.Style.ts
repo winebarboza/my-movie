fe33e9f723b711a5c64531d5d3e3ba8d2e87e95f
winebarboza <wine.barbosa@gmail.com>
2023-08-16 12:54:21 -0300

    refactor - import de Button ao Styled-Components e etilização

--- src/components/movie-card/movieCard.Style.ts
+++ src/components/movie-card/movieCard.Style.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
-import { Card , CardMedia } from '@mui/material'
+import { Card, Button} from '@mui/material'
 
 export const CardStyled = styled(Card)`
     width: 15rem;
     height: 20rem;
     display: flex;
 `
-export const CardMediaStyled = styled(CardMedia)`
+export const ButtonStyled = styled(Button)`
+    
+`
+export const CardMediaStyled = styled.img`
     width: 100%;
+    height: 60%;
     border: 1px solid wihte;
 `
