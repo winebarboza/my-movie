15811f1ed3ba8a05e802343c272291064bb51abe
winebarboza <wine.barbosa@gmail.com>
2023-08-16 22:17:33 -0300

    build - atualizando versão do Typescript

---
+++ src/components/section/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MovieSectionStyled } from './section.style'
+export default function PopularMovie() {
+    return (
+        <div>
+            <MovieSectionStyled>
+                Filmes mais populares
+            </MovieSectionStyled>
+        </div>
+    )
+}
---
+++ src/components/section/section.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { theme } from '../../styles/theme'
+
+export const MovieSectionStyled = styled.h2`
+    color: ${theme.colors.grey};
+    position: relative;
+    border-bottom: 1px solid ${theme.colors.grey};
+    padding-top: 1.5rem;
+    margin-bottom: 1rem;
+    z-index: 2;
+`
