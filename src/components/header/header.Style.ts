9d3a6a16053b73f238b77650769ab6713796d76b
winebarboza <wine.barbosa@gmail.com>
2023-08-16 22:01:13 -0300

    feat - adicionando responsividade ao header e input

--- src/components/header/header.Style.ts
+++ src/components/header/header.Style.ts
@@ -7,6 +7,7 @@ export const HeaderContent = styled.div`
     flex-direction: row;
     padding: 7px;
     height: 3%;
+    
 `;
 export const Div = styled.div`
     width: 38rem;
@@ -18,6 +19,10 @@ export const Div = styled.div`
 
 export const Logo = styled.img`
     width: 8%;
+    @media (max-width: 480px) {
+          width: 15%;
+          margin-right: 8px ;
+        }
 `;
 
 //componentes do materia ui estilizados
@@ -27,6 +32,12 @@ export const InputStyled = styled(Input)`
     width: 30rem;
     background-color: #7e7b7b71;
     border-radius: 4px;
+      @media (max-width: 360px) {
+          width: 12rem;
+        }
+      @media (max-width: 480px) {
+          max-width: 14rem;
+        }
 `;
 
 export const ButtonStyled = styled(Button)`
