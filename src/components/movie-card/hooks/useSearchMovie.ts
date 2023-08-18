5d10b7249f936d17668115ec15db3d285ce0edef
winebarboza <wine.barbosa@gmail.com>
2023-08-18 14:38:54 -0300

    feat - hook personalizado para buscar filmes de acordo com a busca do usuário

---
+++ src/components/movie-card/hooks/useSearchMovie.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { Movie, MovieApiResponse } from "../../../models/movie.interface";
+import api from "../../../providers";
+
+
+export const useSearchMovie = (inputValue: string) => {
+
+    const [moviesFilteredApi, setMoviesFilteredApi] = useState<Movie[]>([]);
+
+    const fetchMovies = async () => {
+        try {
+            const response = await api.get<MovieApiResponse>(`https://api.themoviedb.org/3/search/multi?api_key=8e1d2d15df8ebdf9dc053c78b92de5c3&query=${inputValue}&page=3`);
+            setTimeout(() => {
+                setMoviesFilteredApi(response.data.results)
+            }, 2000);
+        } catch (error) {
+            console.error("error fetching movies", error)
+        }
+    };
+
+    useEffect(() => {
+        fetchMovies();
+    }, [inputValue]);
+    return moviesFilteredApi;
+}
