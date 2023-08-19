c8834fc50b7a88993727fdde0395e15ad938b560
winebarboza <wine.barbosa@gmail.com>
2023-08-19 18:24:53 -0300

    feat - ajustando skeleton dos detalhes do card

--- src/pages/hooks/useGetIdMovie.ts
+++ src/pages/hooks/useGetIdMovie.ts
@@ -10,7 +10,7 @@ const useGetIdMovies = (id: number) => {
         const fetchMoviesDetails = async () => {
             try {
                 const response = await api.get<Movie>( `/movie/${id}?api_key=8e1d2d15df8ebdf9dc053c78b92de5c3`);
-                setMovieDetail(response.data)
+                    setMovieDetail(response.data)
             } catch (error) {
                 console.error("error fetching movies", error)
             }
--- src/pages/movie-details/index.tsx
+++ src/pages/movie-details/index.tsx
@@ -8,6 +8,7 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
 import useGetIdMovies from '../hooks/useGetIdMovie';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -18,13 +19,22 @@ export default function MovieDetailsPage() {
 
     if (!movieDetail) {
         return (
-            <div>
-                <Skeleton variant="rectangular" height={200} />
-                <Skeleton variant="text" width={200} height={30} />
-                <Skeleton variant="text" width={100} height={20} />
-                <Skeleton variant="text" width={300} height={60} />
-                <Skeleton variant="rectangular" height={30} width={120} />
-            </div>
+            <Container>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    minHeight="100vh"
+                >
+                    <div style={{ marginLeft: '10', textAlign: 'center' }}>
+                        <Skeleton variant="rectangular" width={600} height={400} />
+                        <Skeleton variant="text" width={100} height={30} />
+                        <Skeleton variant="text" width={100} height={20} />
+                        <Skeleton variant="text" width={300} height={60} />
+                        <Skeleton variant="rectangular" height={30} width={120} />
+                    </div>
+                </Box>
+            </Container>
         );
     }
     return (
@@ -35,28 +45,30 @@ export default function MovieDetailsPage() {
                 </Link>
             </HeaderContent>
             <Container>
-                <Card sx={{ background: '#434344', maxWidth: 600, margin: '0 auto' }}>
-                    <CardActionArea>
-                        <CardMediaStyled
-                            src={`https://image.tmdb.org/t/p/w500${movieDetail.backdrop_path}`}
-                            alt={movieDetail.title}
-                        />
-                        <CardContent sx={{ color: '#A6A6A6', maxHeight: '80vh', overflow: 'auto' }}>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {movieDetail.original_title}
-                            </Typography>
-                            <Typography variant="body2" color="#808080">
-                                {movieDetail.release_date}
-                            </Typography>
-                            <Typography variant="body2" color="#c5c0c0">
-                                {movieDetail.overview}
-                            </Typography>
-                            {movieDetail.vote_average && (
-                                <Rating name="half-rating-read" value={movieDetail.vote_average} max={10} precision={0.5} readOnly />
-                            )}
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
+                {movieDetail && (
+                    <Card sx={{ background: '#434344', maxWidth: 600, margin: '0 auto' }}>
+                        <CardActionArea>
+                            <CardMediaStyled
+                                src={`https://image.tmdb.org/t/p/w500${movieDetail.backdrop_path}`}
+                                alt={movieDetail.title}
+                            />
+                            <CardContent sx={{ color: '#A6A6A6', maxHeight: '80vh', overflow: 'auto' }}>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {movieDetail.original_title}
+                                </Typography>
+                                <Typography variant="body2" color="#808080">
+                                    {movieDetail.release_date}
+                                </Typography>
+                                <Typography variant="body2" color="#c5c0c0">
+                                    {movieDetail.overview}
+                                </Typography>
+                                {movieDetail.vote_average && (
+                                    <Rating name="half-rating-read" value={movieDetail.vote_average} max={10} precision={0.5} readOnly />
+                                )}
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                )};
             </Container>
         </div>
     );
