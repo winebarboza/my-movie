67f9ed500eb6d3a709be859984bbeb4f10dd6ef0
winebarboza <wine.barbosa@gmail.com>
2023-08-19 15:51:59 -0300

    refactor - retirando breakpoints de exportação

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,5 @@
 export const theme = {
-  breakpoints: {
-    small: '576px',
-    medium: '768px',
-    large: '992px',
-    extraLarge: '1200px',
-  },
-  colors: {
+   colors: {
     white: '#FFFFFF',
     grey: '#A6A6A6',
     backgroundColor: '#1E1E1E',
