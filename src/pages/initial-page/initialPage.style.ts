3d9f3a182b5937d0de51031a75f21a3e1597032c
winebarboza <wine.barbosa@gmail.com>
2023-08-19 11:28:20 -0300

    refactor - identação de código

---
+++ src/pages/initial-page/initialPage.style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+interface BackgroundImgProps {
+    backgroundImage: any;
+}
+
+export const BackgroundImg = styled.div<BackgroundImgProps>`
+ position: relative;
+  width: 100%;
+  height: 100vh;
+
+    background-image: url(${props => props.backgroundImage});
+    background-size: cover;
+    background-position: center;
+    width: 100%;
+    height: 100vh;
+
+    &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
