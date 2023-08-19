a8c9d7487cef1c48940fa44fea4ad0ebc769a70e
winebarboza <wine.barbosa@gmail.com>
2023-08-18 23:57:41 -0300

    refactor - tirado time out de teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import InitialPage from './pages/initialPage';
+import InitialPage from './pages/initial-page/initialPage';
+import MovieDetailsPage from './pages/movie-details/movieDetails';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<InitialPage />} />
+          <Route path='/movie' element={<MovieDetailsPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/movie-card/hooks/useSearchMovie.ts
+++ src/components/movie-card/hooks/useSearchMovie.ts
@@ -10,9 +10,7 @@ export const useSearchMovie = (inputValue: string) => {
     const fetchMovies = async () => {
         try {
             const response = await api.get<MovieApiResponse>(`https://api.themoviedb.org/3/search/multi?api_key=8e1d2d15df8ebdf9dc053c78b92de5c3&query=${inputValue}&page=3`);
-            setTimeout(() => {
-                setMoviesFilteredApi(response.data.results)
-            }, 2000);
+            setMoviesFilteredApi(response.data.results)
         } catch (error) {
             console.error("error fetching movies", error)
         }
